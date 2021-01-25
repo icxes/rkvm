b7123d92da389478ccc87a565be033ed5b463e76
Brendan Ball <ball.brendan50@gmail.com>
2021-01-25 08:18:24 +0200

    create separate mouse and keyboard devices

--- input/src/linux/event_writer.rs
+++ input/src/linux/event_writer.rs
@@ -1,27 +1,29 @@
+use crate::event::Button;
 use crate::event::Event;
+use crate::event::KeyKind;
 use crate::linux::device_id;
 use crate::linux::glue::{self, input_event, libevdev, libevdev_uinput};
 use std::io::{Error, ErrorKind};
 use std::mem::MaybeUninit;
 use std::ops::RangeInclusive;
 
-pub struct EventWriter {
+struct Device {
     evdev: *mut libevdev,
     uinput: *mut libevdev_uinput,
 }
 
-impl EventWriter {
-    pub async fn new() -> Result<Self, Error> {
-        tokio::task::spawn_blocking(Self::new_sync).await?
+impl Device {
+    pub async fn new(event_types: &'static [(u32, &[RangeInclusive<u32>])]) -> Result<Self, Error> {
+        tokio::task::spawn_blocking(move || Self::new_sync(event_types)).await?
     }
 
-    fn new_sync() -> Result<Self, Error> {
+    fn new_sync(event_types: &'static [(u32, &[RangeInclusive<u32>])]) -> Result<Self, Error> {
         let evdev = unsafe { glue::libevdev_new() };
         if evdev.is_null() {
             return Err(Error::new(ErrorKind::Other, "Failed to create device"));
         }
 
-        if let Err(err) = unsafe { setup_evdev(evdev) } {
+        if let Err(err) = unsafe { setup_evdev(evdev, event_types) } {
             unsafe {
                 glue::libevdev_free(evdev);
             }
@@ -51,7 +53,7 @@ impl EventWriter {
         self.write_raw(event.to_raw())
     }
 
-    pub(crate) fn write_raw(&mut self, event: input_event) -> Result<(), Error> {
+    fn write_raw(&mut self, event: input_event) -> Result<(), Error> {
         // As far as tokio is concerned, the FD never becomes ready for writing, so just write it normally.
         // If an error happens, it will be propagated to caller and the FD is opened in nonblocking mode anyway,
         // so it shouldn't be an issue.
@@ -79,31 +81,17 @@ impl EventWriter {
     }
 }
 
-impl Drop for EventWriter {
-    fn drop(&mut self) {
-        unsafe {
-            glue::libevdev_uinput_destroy(self.uinput);
-            glue::libevdev_free(self.evdev);
-        }
-    }
-}
-
-unsafe impl Send for EventWriter {}
-
-const TYPES: &[(u32, &[RangeInclusive<u32>])] = &[
-    (glue::EV_SYN, &[glue::SYN_REPORT..=glue::SYN_REPORT]),
-    (glue::EV_REL, &[0..=glue::REL_MAX]),
-    (glue::EV_KEY, &[0..=glue::KEY_MAX]),
-];
-
-unsafe fn setup_evdev(evdev: *mut libevdev) -> Result<(), Error> {
+unsafe fn setup_evdev(
+    evdev: *mut libevdev,
+    event_types: &'static [(u32, &[RangeInclusive<u32>])],
+) -> Result<(), Error> {
     glue::libevdev_set_name(evdev, b"rkvm\0".as_ptr() as *const _);
     glue::libevdev_set_id_vendor(evdev, device_id::VENDOR as _);
     glue::libevdev_set_id_product(evdev, device_id::PRODUCT as _);
     glue::libevdev_set_id_version(evdev, device_id::VERSION as _);
     glue::libevdev_set_id_bustype(evdev, glue::BUS_USB as _);
 
-    for (r#type, codes) in TYPES.iter().copied() {
+    for (r#type, codes) in event_types.iter().copied() {
         let ret = glue::libevdev_enable_event_type(evdev, r#type);
         if ret < 0 {
             return Err(Error::from_raw_os_error(-ret));
@@ -118,4 +106,88 @@ unsafe fn setup_evdev(evdev: *mut libevdev) -> Result<(), Error> {
     }
 
     Ok(())
+}
+
+impl Drop for Device {
+    fn drop(&mut self) {
+        unsafe {
+            glue::libevdev_uinput_destroy(self.uinput);
+            glue::libevdev_free(self.evdev);
+        }
+    }
+}
+
+unsafe impl Send for Device {}
+
+struct Mouse {
+    device: Device,
+}
+
+const MOUSE_EVENT_TYPES: &[(u32, &[RangeInclusive<u32>])] = &[
+    (glue::EV_SYN, &[glue::SYN_REPORT..=glue::SYN_REPORT]),
+    (glue::EV_REL, &[0..=glue::REL_MAX]),
+    (glue::EV_KEY, &[glue::BTN_LEFT..=glue::BTN_MIDDLE]),
+];
+
+impl Mouse {
+    pub async fn new() -> Result<Self, Error> {
+        let device = Device::new(MOUSE_EVENT_TYPES).await?;
+        Ok(Self { device })
+    }
+
+    pub async fn write(&mut self, event: Event) -> Result<(), Error> {
+        self.device.write(event).await?;
+        Ok(())
+    }
+}
+
+struct Keyboard {
+    device: Device,
+}
+
+const KEYBOARD_EVENT_TYPES: &[(u32, &[RangeInclusive<u32>])] = &[
+    (glue::EV_SYN, &[glue::SYN_REPORT..=glue::SYN_REPORT]),
+    (glue::EV_KEY, &[glue::KEY_ESC..=glue::KEY_SCROLLLOCK]),
+];
+
+impl Keyboard {
+    pub async fn new() -> Result<Self, Error> {
+        let device = Device::new(KEYBOARD_EVENT_TYPES).await?;
+        Ok(Self { device })
+    }
+
+    pub async fn write(&mut self, event: Event) -> Result<(), Error> {
+        self.device.write(event).await?;
+        Ok(())
+    }
+}
+
+pub struct EventWriter {
+    mouse: Mouse,
+    keyboard: Keyboard,
+}
+
+impl EventWriter {
+    pub async fn new() -> Result<Self, Error> {
+        let mouse = Mouse::new().await?;
+        let keyboard = Keyboard::new().await?;
+        Ok(Self { mouse, keyboard })
+    }
+
+    pub async fn write(&mut self, event: Event) -> Result<(), Error> {
+        match event {
+            Event::MouseScroll { delta } => self.mouse.write(Event::MouseScroll { delta }).await,
+            Event::MouseMove { axis, delta } => {
+                self.mouse.write(Event::MouseMove { axis, delta }).await
+            }
+            Event::Key { direction, kind } => match kind {
+                KeyKind::Button(Button::Left)
+                | KeyKind::Button(Button::Right)
+                | KeyKind::Button(Button::Middle) => {
+                    self.mouse.write(Event::Key { direction, kind }).await
+                }
+                _ => self.keyboard.write(Event::Key { direction, kind }).await,
+            },
+        }
+    }
 }
