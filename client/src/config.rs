7f9bb310388bf94b619bccf4058a5be25c7c32e8
Leonardo Lang <leonardo.lang@gmail.com>
2022-04-08 00:57:19 -0400

    Support for multiple server on client config
    Client config now requires a section name on the configuration,
    with multiple sections used for multiple server definitions.
    
    Connections are attempted on all servers at once, and the first
    to succeed is returned.

--- client/src/config.rs
+++ client/src/config.rs
@@ -2,32 +2,36 @@ use serde::de::{self, Visitor};
 use serde::{Deserialize, Deserializer};
 use std::fmt::{self, Formatter};
 use std::path::PathBuf;
+use std::collections::HashMap;
 
-#[derive(Deserialize)]
+pub type Config = HashMap<String, Server>;
+
+#[derive(Deserialize, Clone)]
 #[serde(rename_all = "kebab-case")]
-pub struct Config {
-    pub server: Server,
+pub struct Server {
+    pub server_address: ServerAddress,
     pub certificate_path: PathBuf,
 }
 
-pub struct Server {
-    pub hostname: String,
+#[derive(Clone)]
+pub struct ServerAddress {
+    pub host: String,
     pub port: u16,
 }
 
-impl<'de> Deserialize<'de> for Server {
+impl<'de> Deserialize<'de> for ServerAddress {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
     {
-        deserializer.deserialize_str(ServerVisitor)
+        deserializer.deserialize_str(ServerAddressVisitor)
     }
 }
 
-struct ServerVisitor;
+struct ServerAddressVisitor;
 
-impl<'de> Visitor<'de> for ServerVisitor {
-    type Value = Server;
+impl<'de> Visitor<'de> for ServerAddressVisitor {
+    type Value = ServerAddress;
 
     fn expecting(&self, f: &mut Formatter) -> fmt::Result {
         write!(f, "a server description (hostname:port)")
@@ -40,7 +44,7 @@ impl<'de> Visitor<'de> for ServerVisitor {
         let err = || E::custom("Invalid server description");
 
         let mut split = data.split(':');
-        let hostname = split.next().ok_or_else(err)?;
+        let host = split.next().ok_or_else(err)?;
         let port = split
             .next()
             .and_then(|data| data.parse().ok())
@@ -50,8 +54,8 @@ impl<'de> Visitor<'de> for ServerVisitor {
             return Err(E::custom("Extraneous data"));
         }
 
-        Ok(Server {
-            hostname: hostname.to_owned(),
+        Ok(ServerAddress {
+            host: host.to_owned(),
             port,
         })
     }
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,12 +1,12 @@
 mod config;
 
 use anyhow::{Context, Error};
-use config::Config;
+use config::{Config, Server};
 use input::EventWriter;
 use log::LevelFilter;
 use net::{self, Message, PROTOCOL_VERSION};
 use std::convert::Infallible;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::process;
 use structopt::StructOpt;
 use tokio::fs;
@@ -14,14 +14,36 @@ use tokio::io::BufReader;
 use tokio::net::TcpStream;
 use tokio::time;
 use tokio_native_tls::native_tls::{Certificate, TlsConnector};
+use futures::{future::select_all, FutureExt};
 
-async fn run(server: &str, port: u16, certificate_path: &Path) -> Result<Infallible, Error> {
-    let certificate = fs::read(certificate_path)
-        .await
+async fn try_connect(name: String, server: Server) -> Result<(String, Server, Certificate, TcpStream), Error> {
+    let certificate = fs::read(&server.certificate_path).await
         .context("Failed to read certificate")?;
+
     let certificate = Certificate::from_der(&certificate)
         .or_else(|_| Certificate::from_pem(&certificate))
         .context("Failed to parse certificate")?;
+
+    let (host, port) = (&server.server_address.host, server.server_address.port);
+
+    log::info!("Attempting connection to {} ({}:{})", name, host, port);
+    let stream = TcpStream::connect((host.as_str(), port)).await?;
+
+    Ok((name, server, certificate, stream))
+}
+
+async fn run(mut config: Config) -> Result<Infallible, Error> {
+    let (name, server, certificate, stream) = {
+        let (res, _num, _vec) = select_all(config.drain().map(|(name, srv)| {
+            try_connect(name.to_string(), srv).boxed()
+        })).await;
+        res?
+    };
+
+    let (host, port) = (&server.server_address.host, server.server_address.port);
+
+    log::debug!("Connection open to {} ({}:{}), setting up TLS: {} ", name, host, port,
+        server.certificate_path.to_string_lossy());
 
     let connector: tokio_native_tls::TlsConnector = TlsConnector::builder()
         .add_root_certificate(certificate)
@@ -29,17 +51,17 @@ async fn run(server: &str, port: u16, certificate_path: &Path) -> Result<Infalli
         .context("Failed to create connector")?
         .into();
 
-    let stream = TcpStream::connect((server, port)).await?;
     if let Err(err) = stream.set_nodelay(true) {
         log::warn!("setting TCP_NODELAY failed: {}", err);
     };
+
     let stream = BufReader::new(stream);
     let mut stream = connector
-        .connect(server, stream)
+        .connect(&server.server_address.host, stream)
         .await
         .context("Failed to connect")?;
 
-    log::info!("Connected to {}:{}", server, port);
+    log::info!("Connected to {} ({}:{})", name, host, port);
 
     net::write_version(&mut stream, PROTOCOL_VERSION).await?;
 
@@ -104,7 +126,7 @@ async fn main() {
     };
 
     tokio::select! {
-        result = run(&config.server.hostname, config.server.port, &config.certificate_path) => {
+        result = run(config.clone()) => {
             if let Err(err) = result {
                 log::error!("Error: {:#}", err);
                 process::exit(1);
